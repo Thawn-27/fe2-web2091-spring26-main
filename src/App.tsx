b37cdb63d241f8993eabe4497fd5fcac4f4186e8
thanh <pxthanh2742006@gmail.com>
2026-03-17 15:35:47 +0700

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Layout } from "antd";
+import { Link, Routes, Route } from "react-router-dom";
+import Lab3 from "./lab3";
+import Lab4 from "./lab4";
 import { Toaster } from "react-hot-toast";
-import { Link } from "react-router-dom";
-import { Button, Layout } from "antd";
-import Lab3 from "./Lab3";
 
 const { Header, Content, Footer } = Layout;
 
@@ -11,31 +12,26 @@ function App() {
       {/* Navbar */}
       <nav className="bg-blue-600 text-white shadow">
         <div className="max-w-6xl mx-auto px-4 h-16 flex items-center justify-between">
-
           <Link to="/" className="text-xl font-semibold">
             WEB2091 App
           </Link>
 
-          <div className="hidden md:flex items-center space-x-8">
-            <Link to="/" className="hover:text-gray-200">Trang chủ</Link>
-            <Link to="/list" className="hover:text-gray-200">Danh sách</Link>
-            <Link to="/add" className="hover:text-gray-200">Thêm mới</Link>
+          <div className="flex gap-4">
+            <Link to="/">Danh sách</Link>
+            <Link to="/add">Thêm mới</Link>
           </div>
-
-          <div className="hidden md:flex items-center space-x-5">
-            <Link to="/" className="hover:text-gray-200">Đăng nhập</Link>
-            <Link to="/" className="hover:text-gray-200">Đăng ký</Link>
-          </div>
-
         </div>
       </nav>
 
       {/* Layout */}
-      <Layout style={{ margin: 20 }}>
+      <Layout style={{ marginTop: 20 }}>
         <Header style={{ color: "white" }}>Header</Header>
 
         <Content style={{ padding: 20 }}>
-          <Lab3 />
+          <Routes>
+            <Route path="/" element={<Lab3 />} />
+            <Route path="/add" element={<Lab4 />} />
+          </Routes>
         </Content>
 
         <Footer>Footer</Footer>
